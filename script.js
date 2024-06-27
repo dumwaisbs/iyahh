773ae59735a5f52fa7d16db39a90aeb55848688a
dumwaisbs <dumwaisbs@gmail.com>
2024-06-27 21:21:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('forgiveBtn').addEventListener('click', function() {
+    document.querySelector('.card').classList.add('hidden');
+    document.getElementById('message').classList.remove('hidden');
+});
